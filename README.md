8a634a4067a9c89ef7d6558efbdc9a857c97be89
S. Scott Collis <flow.physics.simulation@gmail.com>
2022-09-02 09:01:02 -0600

    major update

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ To clone all FPS codes from GitHub and build all:
 ```bash
 cd $FPSROOT
 $FPSSCRIPTS/fps-clone.sh 
-$FPSSCRIPTS./fps-build.sh CC=gcc-12
+$FPSSCRIPTS/fps-build.sh CC=gcc-12
 ```
 Where in this case, we have set the CC compiler to be `gcc-12` as needed
 on recent Mac profiles when using HomeBrew GCC.
@@ -24,6 +24,23 @@ where you have cloned the `fps-scripts` repo using something like:
 ```bash
 git clone git@github.com:flow-physics-simulation/fps-scripts.git
 export FPSSCRIPTS=`pwd`/fps-scripts
+```
+If running on AppleSilicon, you would use:
+```bash
+$FPSSCRIPTS/fps-build.sh USE_APPLEBREW_OPENBLAS=1 CC=gcc-12 \
+USE_NR=1 LIBNR_DIR=$HOME/fps/NR-utilities
+```
+where I have set the path for Numerical Recepies (optional) utilities if you
+happen to have a valid licence to use them.
+
+```bash
+./fps-scripts/fps-build.sh -n -- USE_APPLEBREW_OPENBLAS=1 CC=gcc-12 LIBNR_DIR=$HOME/fps/NR-utilities
+```
+
+If on Linux, HomeBrew install yet again in another location, so you would use
+```bash
+$FPSSCRIPTS/fps-build.sh USE_LINUXBREW_OPENBLAS=1 \
+LIBNR_DIR=$HOME/fps/NR-utilities
 ```
 
 S. Scott Collis\
--- fps-build.sh
+++ fps-build.sh
@@ -1,14 +1,70 @@
 #!/bin/bash
+#===================================================================
+#
+# Builds all FPS codes
+#
+# Uses Numerical-Recipes routines if you have them and are licenced
+# For codes that need BLAS, we use OpenBLAS and define
+#   1) Linux:          USE_LINUXBREW_OPENBLAS=1
+#   2) Apple Intel     USE_HOMEBREW_OPENBLAS=1
+#   3) Apple Silicon:  USE_APPLEBREW_OPENBLAS=1
+#
+# The path to Numerical-Recipes is set with:  LIBNR_DIR
+#
+# Finally, on MacOS you may need to specify which CC compiler such
+# as CC=gcc-12
+#
+# Author:  S. Scott Collis
+# Email:   flow.phyisics.simulation@gmail.com
+#
+#===================================================================
 set -e
+OPTS=$(getopt n,r,h $*)
+#echo $OPTS
+eval set -- "$OPTS"
+while :
+do
+  case "$1" in
+    -h )
+      echo "Usage:  fps-build.sh -n -h -- <args>"
+      echo "  -n | -r : Turns Numerical-Recipes" 
+      echo "  -h : This help"
+      exit 2
+      ;;
+    -n | -r )
+      FPS_USE_NR=1
+      shift 1
+      ;;
+    -- )
+      shift;
+      break
+      ;;
+    * )
+      echo "unexpected option: $1"
+      ;;
+  esac
+done
+#echo FPS_USE_NR = $FPS_USE_NR
+if [[ $FPS_USE_NR -eq 1 ]] ; then
+echo '============================================================='
+echo '   Using Numerical Recepies (must be licenced)'
+echo '     This activates builds for:  stab, shoot'
+echo '     Make sure to set LIBNR_DIR as needed'
+echo '============================================================='
+fi
 #
-# Need NR for Stab, Shoot, LNS3D/mesh
+# Need NR for Stab, Shoot, LNS3D/mesh, LNS3D/util
 #
-cd NR-utilities
-\ln -fs gcc.mak Makefile
-make clean && make
-
-cd ../lns3d
-./build.sh $@
+if [[ $FPS_USR_NR -eq 1 ]] ; then
+  cd NR-utilities
+  \ln -fs gcc.mak Makefile
+  make clean && make
+  cd ../lns3d
+  ./build.sh USE_NR=1 $@
+else
+  cd lns3d
+  ./build.sh $@
+fi
 
 cd ../fsc
 \ln -fs gcc.mak Makefile
@@ -19,21 +75,23 @@ cd ../npot/src
 make clean && make $@
 cd ..
 
-cd ../stab
-\ln -fs gcc.mak Makefile
-make clean && make $@
+if [[ $FPS_USR_NR -eq 1 ]] ; then
+  cd ../stab
+  \ln -fs gcc.mak Makefile
+  make clean && make USE_NR=1 $@
 
-cd ../shoot
-\ln -fs gcc.mak Makefile
-make clean && make $@
+  cd ../shoot
+  \ln -fs gcc.mak Makefile
+  make clean && make USE_NR=1 $@
+
+  cd ../compbl
+  \ln -fs gcc.mak Makefile
+  make clean && make $@
+fi
 #
-# Less needed
+#  
 #
 cd ../os-stab
-\ln -fs gcc.mak Makefile
-make clean && make $@
-
-cd ../compbl
 \ln -fs gcc.mak Makefile
 make clean && make $@
 
