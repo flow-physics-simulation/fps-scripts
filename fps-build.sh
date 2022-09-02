43a17509c3513ab630af7869fa5449c3a506019b
S. Scott Collis <flow.physics.simulation@gmail.com>
2022-09-01 23:05:29 -0600

    Add sym links to Makefile

--- fps-build.sh
+++ fps-build.sh
@@ -4,29 +4,37 @@ set -e
 # Need NR for Stab, Shoot, LNS3D/mesh
 #
 cd NR-utilities
+\ln -fs gcc.mak Makefile
 make clean && make
 
 cd ../lns3d
 ./build.sh $@
 
 cd ../fsc
+\ln -fs gcc.mak Makefile
 make clean && make $@
 
 cd ../npot/src
+\ln -fs gcc.mak Makefile
 make clean && make $@
+cd ..
 
 cd ../stab
+\ln -fs gcc.mak Makefile
 make clean && make $@
 
 cd ../shoot
+\ln -fs gcc.mak Makefile
 make clean && make $@
 #
 # Less needed
 #
 cd ../os-stab
+\ln -fs gcc.mak Makefile
 make clean && make $@
 
 cd ../compbl
+\ln -fs gcc.mak Makefile
 make clean && make $@
 
 cd ../..
